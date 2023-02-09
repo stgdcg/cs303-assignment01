d108cd0073547bb654ba10cd7234ee37743e9618
Seth Grisham <sethgrisham@protonmail.com>
2023-02-09 14:36:49 -0600

    Increase input size, move functions out of main

--- array_helpers.h
+++ array_helpers.h
@@ -1,5 +1,9 @@
+#pragma once
 #include "array.h"
 #include <iostream>
 
 using namespace std;
 
+void input_file(string file, array &arr);
+
+char prompt_options();
--- main.cpp
+++ main.cpp
@@ -1,39 +1,11 @@
 #include "array.h"
+#include "array_helpers.h"
 #include <iostream>
 #include <fstream>
 #include <stdexcept>
 #include <string>
 
 using namespace std;
-
-void input_file(string file, array &arr) {
-  ifstream fin(file);
-  if (!fin.is_open()) {
-    throw runtime_error("File not found");
-  }
-
-  int tmp;
-  while (fin >> tmp) {
-    arr.push_back(tmp);
-  }
-
-  fin.close();
-}
-
-char prompt_options() {
-  char input;
-  cout << "ENTER OPTION:" << endl;
-  cout << "(S)earch array and return index of number (-1 if not there)" << endl;
-  cout << "(R)eplace a value at an index" << endl;
-  cout << "(A)dd a new value at the end of the array" << endl;
-  cout << "(D)elete a value at an index" << endl;
-  cout << "(P)rint the array" << endl;
-  cout << "(Q)uit" << endl;
-  cout << " >> ";
-  cin >> input;
-  input = toupper(input);
-  return input;
-}
 
 int main() {
 
@@ -73,7 +45,7 @@ int main() {
       cout << "\tEnter a value to replace: ";
       cin >> value;
 
-      arr.modify(index, value);
+      cout << "\tThe replaced value was: " << arr.modify(index, value) << endl;
       
       break;
     case 'A':
